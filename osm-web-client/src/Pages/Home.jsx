ac88d82a276697f2c235274aca94bd40cbb3e963
XpoizonEximus <serenus222@gmail.com>
2022-09-16 18:16:15 +0300

    * Create Folder Hierarchy * Create Minimalist HomePage

--- osm-web-client/src/Pages/Home.jsx
+++ osm-web-client/src/Pages/Home.jsx
@@ -1,23 +1,15 @@
 import React from "react";
-import logo from "../logo.svg";
+import banner from "../Assets/Photos/banner.jpg"
 import "../App.css";
 
 const HomePage = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={banner} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          ORGANIZATIA STUDENTILOR MEDICINISTI DIN CLUJ
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
